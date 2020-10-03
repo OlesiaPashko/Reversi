46781225be2c5035dce7c66552625438c7a10140
OlesiaPashko <pashkoolesia1234@gmail.com>
2020-10-03 22:14:14 +0300

    add finish scene

--- Assets/Views/FinishSceneManager.cs
+++ Assets/Views/FinishSceneManager.cs
@@ -4,10 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FinishSceneManager : MonoBehaviour
 {
     public static int FirstPlayerScore;
     public static int SecondPlayerScore;
 
+    public Text WinnerText;
+    public Text FirstPlayerScoreText;
+    public Text SecondPlayerScoreText;
+
+    private void Start()
+    {
+        if(FirstPlayerScore>SecondPlayerScore)
+            WinnerText.text += " First Player";
+        else if (SecondPlayerScore > FirstPlayerScore)
+        {
+            WinnerText.text += " Second Player";
+        }
+        else
+        {
+            WinnerText.text += " Friendship";
+        }
+        FirstPlayerScoreText.text += " " + FirstPlayerScore;
+        SecondPlayerScoreText.text += " " + SecondPlayerScore;
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
 }
